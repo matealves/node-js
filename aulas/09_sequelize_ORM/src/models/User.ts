ef6880e4a50420bdee95610726293c10e12daa6d
Mateus Alves <contatomateusalves@hotmail.com>
2024-02-12 12:04:48 -0300

    ⚙️criando conexão com o banco com ORM Sequelize

---
+++ aulas/09_sequelize_ORM/src/models/User.ts
@@ -0,0 +1,29 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../connection/mysql";
+
+export interface UserInstance extends Model {
+  id: number;
+  name: string;
+  age: number;
+}
+
+export const User = sequelize.define<UserInstance>(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      defaultValue: 18,
+    },
+  },
+  {
+    tableName: "users",
+    timestamps: false,
+  }
+);
