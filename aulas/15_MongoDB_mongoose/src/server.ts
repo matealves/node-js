e7f87b74138d60171af1cd030e12bfd3eaf347bb
Mateus Alves <contatomateusalves@hotmail.com>
2024-02-22 22:43:17 -0300

    ⚙️criando MongoDB e conexão com o Mongoose

---
+++ aulas/15_MongoDB_mongoose/src/server.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response } from "express";
+import path from "path";
+import mustache from "mustache-express";
+import dotenv from "dotenv";
+import { mongoDBConnect } from "./database/mongodb";
+import mainRoutes from "./routes/index";
+
+dotenv.config();
+
+// conectar com o banco de dados
+mongoDBConnect();
+
+const server = express();
+const PORT = process.env.PORT;
+
+server.set("view engine", "mustache");
+server.set("views", path.join(__dirname, "views"));
+server.engine("mustache", mustache());
+
+server.use(express.static(path.join(__dirname, "../public")));
+
+server.use(express.urlencoded({ extended: true }));
+
+server.use(mainRoutes);
+
+server.use((req: Request, res: Response) => {
+  res.status(404).send("Página não encontrada!");
+});
+
+// $ npm start
+server.listen(PORT, () => {
+  console.log(`\x1b[36m[PORT:${PORT}] \x1b[32mApp Express iniciado...\x1b[0m`);
+  console.log(
+    `\x1b[32mServer local:\x1b[0m \x1b[36mhttp://127.0.0.1:${PORT}\x1b[0m`
+  );
+});
