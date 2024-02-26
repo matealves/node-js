c47aedbac6b428533524cc0ae498b627027725b1
Mateus Alves <contatomateusalves@hotmail.com>
2024-02-26 14:30:09 -0300

    ⚙️oredenando dados com o método sort

--- aulas/16_MongoDB_CRUD/src/controllers/homeController.ts
+++ aulas/16_MongoDB_CRUD/src/controllers/homeController.ts
@@ -2,10 +2,9 @@ import { Request, Response } from "express";
 
 import { Product } from "../models/Product";
 import User from "../models/User";
-import isEmail from "validator/lib/isEmail";
 
 export const home = async (req: Request, res: Response) => {
-  // GET
+  // #### GET
   // const users = await User.find(); //return Array
   // const users = await User.findById("65d7e7b12af5b20e2986994a"); //return Object
   // const users = await User.findOne({ email: "ph@email.com.br" }); //return Object
@@ -16,12 +15,18 @@ export const home = async (req: Request, res: Response) => {
 
   // const users = await User.find({ age: { $gt: 24 } }); // > 24
   // const users = await User.find({ age: { $lte: 25 } }); // <= 25
-  const users = await User.find({ age: { $gt: 24, $lt: 30 } }); // > 24 & < 30
-  console.log("users:", users);
+  // const users = await User.find({ age: { $gt: 24, $lt: 30 } }); // > 24 & < 30
 
-  // POST
-  // PUT/UPDATE
-  // DELETE
+  // #### SORT / ORDER
+  // (1) ASC: ordem crescente ----- (-1) DESC: ordem decrescente
+  // const users = await User.find({ age: { $gt: 18 } }).sort({ age: -1 });
+  const users = await User.find().sort({ "name.firstName": 1 });
+
+  // #### POST
+  // #### PUT/UPDATE
+  // #### DELETE
+
+  console.log("users:", users);
 
   let age: number = 90;
   let showOld: boolean = false;
