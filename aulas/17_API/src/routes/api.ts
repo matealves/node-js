d7ce7e1c2130900a31cfa6819bf069a736dc6a50
Mateus Alves <contatomateusalves@hotmail.com>
2024-02-29 16:22:33 -0300

    ⚙️planejando API e criando arquitetura do projeto

--- aulas/17_API/src/routes/api.ts
+++ aulas/17_API/src/routes/api.ts
@@ -1,25 +1,12 @@
 import { Router } from "express";
 
+import * as ApiController from "../controllers/apiController";
+
 const router = Router();
 
 // GET
-router.get("/ping", (req, res) => {
-  res.json({ pong: true });
-});
-
-router.get("/random", (req, res) => {
-  const nRand: number = Math.floor(Math.random() * 100);
-  res.json({ number: nRand });
-});
-
-router.get("/nome/:nome", (req, res) => {
-  function capitalize(nome: string) {
-    return nome.charAt(0).toUpperCase() + nome.slice(1);
-  }
-
-  const nome: string = req.params.nome;
-
-  res.json({ nome: capitalize(nome) });
-});
+router.get("/ping", ApiController.ping);
+router.get("/random", ApiController.random);
+router.get("/nome/:nome", ApiController.nome);
 
 export default router;
