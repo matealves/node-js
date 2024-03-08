3dbc36267463ee4b46df3dd0681416f32d3011de
Mateus Alves <contatomateusalves@hotmail.com>
2024-03-08 14:31:33 -0300

    ⚙️criando API base de registro e login

---
+++ aulas/19_API_auth/src/routes/routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+import * as ApiController from "../controllers/apiController";
+
+const router = Router();
+
+// Test
+router.get("/ping", ApiController.ping);
+
+router.post("/register", ApiController.register);
+router.post("/login", ApiController.login);
+
+router.get("/list", ApiController.list);
+
+export default router;
