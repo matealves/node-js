c694f229f7eba135cb7aff25c42c017914847868
Mateus Alves <contatomateusalves@hotmail.com>
2024-03-27 21:07:54 -0300

    ⚙️unitary tests with Jest

---
+++ aulas/22_Test_Jest/src/controllers/api.controller.ts
@@ -0,0 +1,90 @@
+import { Request, Response } from "express";
+import JWT from "jsonwebtoken";
+import dotenv from "dotenv";
+import { User } from "../models/User";
+
+dotenv.config();
+
+// Test
+export const ping = (req: Request, res: Response) => {
+  res.json({ pong: true });
+};
+
+export const register = async (req: Request, res: Response) => {
+  const { name, lastName, email, password } = req.body;
+
+  if (name && lastName && email && password) {
+    const hasUser = await User.findOne({ where: { email } });
+
+    if (!hasUser) {
+      const newUser = await User.create({
+        name,
+        lastName,
+        email,
+        password,
+      });
+
+      const token = JWT.sign(
+        {
+          id: newUser.id,
+          name: newUser.name,
+          lastName: newUser.lastName,
+          email: newUser.email,
+        },
+        process.env.JWT_SECRET_KEY as string,
+        { expiresIn: "2h" }
+      );
+
+      res.status(201);
+      res.json({
+        id: newUser.id,
+        message: "Usuário cadastrado com sucesso!",
+        token,
+      });
+    } else {
+      res.json({ error: "E-mail já existe." });
+    }
+  } else {
+    res.json({ error: "Dados obrigatórios não enviados." });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  if (req.body.email && req.body.password) {
+    const email: string = req.body.email;
+    const password: string = req.body.password;
+
+    const user = await User.findOne({
+      where: { email, password },
+    });
+
+    if (user) {
+      const token = JWT.sign(
+        {
+          id: user.id,
+          name: user.name,
+          lastName: user.lastName,
+          email: user.email,
+        },
+        process.env.JWT_SECRET_KEY as string,
+        { expiresIn: "2h" }
+      );
+
+      res.json({ status: true, token });
+      return;
+    }
+  }
+
+  res.json({ status: false });
+};
+
+export const list = async (req: Request, res: Response) => {
+  const users = await User.findAll();
+  const list: string[] = [];
+
+  for (const i in users) {
+    list.push(`${users[i].name} ${users[i].lastName}`);
+  }
+
+  res.json({ list });
+};
---
+++ aulas/22_Test_Jest/src/libs/Math.test.ts
@@ -0,0 +1,26 @@
+import { Math } from "./Math";
+
+describe("Testing Math library", () => {
+  it("should sum two numbers correctly", () => {
+    const response = Math.sum(1, 2);
+    expect(response).toBe(3);
+  });
+
+  it("should subtract two numbers correctly", () => {
+    const response = Math.sub(4, 2);
+    expect(response).toBe(2);
+  });
+
+  it("should multiply two numbers correctly", () => {
+    const response = Math.mult(3, 5);
+    expect(response).toBe(15);
+  });
+
+  it("should divide two numbers correctly", () => {
+    const response = Math.div(30, 3);
+    expect(response).toBe(10);
+
+    const response2 = Math.div(3, 0);
+    expect(response2).toBe(false);
+  });
+});
