9b68b15490b830cdeb8b3819f725e501469f8484
Mateus Alves <contatomateusalves@hotmail.com>
2024-04-01 19:25:33 -0300

    ⚙️some Matches

--- aulas/22_Test_Jest/src/libs/Math.test.ts
+++ aulas/22_Test_Jest/src/libs/Math.test.ts
@@ -18,9 +18,50 @@ describe("Testing Math library", () => {
 
   it("should divide two numbers correctly", () => {
     const response = Math.div(30, 3);
-    expect(response).toBe(10);
+    expect(response).toEqual(10);
 
     const response2 = Math.div(3, 0);
-    expect(response2).toBe(false);
+    // expect(response2).toBe(false);
+    expect(response2).toBeFalsy();
+  });
+
+  // test others Matches
+  it("contar quantos caracteres tem na string", () => {
+    const response = "Mateus";
+    expect(response).toHaveLength(6);
+  });
+
+  it("se possui propriedade email", () => {
+    const response = {
+      name: "Mateus",
+      email: "test@example.com",
+    };
+
+    // expect(response).toHaveProperty("email");
+    // expect(response).not.toBeNull();
+    expect(response).not.toBeUndefined();
+  });
+
+  it("se é maior que 15", () => {
+    const response = 20;
+    // expect(response).toBeGreaterThan(15);
+    expect(response).toBeGreaterThanOrEqual(15);
+    expect(response).toBeLessThan(21);
+  });
+
+  it("se é um e-mail (REGEX)", () => {
+    const response = "test@example.com";
+
+    expect(response).toMatch(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  });
+
+  it("retornar erro", () => {
+    const response = Math.div(30, 3);
+    expect(response).toEqual(10);
+
+    const response2 = Math.div(3, 0);
+    expect(response2).toThrow(new Error("Não divide por zero"));
   });
 });
