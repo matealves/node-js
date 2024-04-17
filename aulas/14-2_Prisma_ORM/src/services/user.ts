6db79684b2f5156188236a22f02b54c91b565565
Mateus Alves <contatomateusalves@hotmail.com>
2024-04-17 20:21:17 -0300

    ⚙️types of Prisma

--- aulas/14-2_Prisma_ORM/src/services/user.ts
+++ aulas/14-2_Prisma_ORM/src/services/user.ts
@@ -1,20 +1,10 @@
 import { Prisma } from "@prisma/client";
 import { prisma } from "../libs/prisma";
 
-type createUserProps = {
-  name: string;
-  email: string;
-};
-
-export const createUser = async ({ name, email }: createUserProps) => {
+export const createUser = async (data: Prisma.UserCreateInput) => {
   try {
-    return await prisma.user.create({
-      data: { name, email },
-    });
+    return await prisma.user.create({ data });
   } catch (error) {
-    // if (error instanceof Prisma.PrismaClientKnownRequestError) {
-    //     if(error.code === 'P2002') {}
-    // }
     return false;
   }
 };
