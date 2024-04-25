6f7fd3c3bbcadbc58025b4dc41ba674051076fda
Mateus Alves <contatomateusalves@hotmail.com>
2024-04-24 22:15:59 -0300

    feat: add CRUD in controller

--- projetos/api-adonisjs/app/controllers/moments_controller.ts
+++ projetos/api-adonisjs/app/controllers/moments_controller.ts
@@ -3,6 +3,12 @@ import app from '@adonisjs/core/services/app'
 import { v4 as uuidv4 } from 'uuid'
 import Moment from '#models/moment'
 
+interface UpdateMomentRequestBody {
+  title?: string
+  description?: string
+  image?: string
+}
+
 export default class MomentsController {
   private validationOptions = {
     types: ['image'],
@@ -11,12 +17,11 @@ export default class MomentsController {
 
   public async store({ request, response }: HttpContext) {
     const body = request.body()
-    const moment = await Moment.create(body)
-
     const image = request.file('image', this.validationOptions)
 
     if (image) {
       const imageName = `${uuidv4()}.${image.extname}`
+
       await image.move(app.tmpPath('uploads'), {
         name: imageName,
       })
@@ -24,9 +29,64 @@ export default class MomentsController {
       body.image = imageName
     }
 
+    const moment = await Moment.create(body)
+
     response.status(201)
     return {
       message: 'Momento criado com sucesso!',
+      data: moment,
+    }
+  }
+
+  public async index() {
+    const moments = await Moment.all()
+    return { data: moments }
+  }
+
+  public async show({ params }: HttpContext) {
+    const moment = await Moment.findOrFail(params.id)
+    return { data: moment }
+  }
+
+  public async destroy({ params }: HttpContext) {
+    const moment = await Moment.findOrFail(params.id)
+    await moment.delete()
+
+    return {
+      message: 'Momento excluído com sucesso!',
+      moment,
+    }
+  }
+
+  public async update({ params, request }: HttpContext) {
+    const body = request.body as UpdateMomentRequestBody
+    const moment = await Moment.findOrFail(params.id)
+
+    if (body.title !== undefined) {
+      moment.title = body.title
+    }
+    if (body.description !== undefined) {
+      moment.description = body.description
+    }
+
+    if (moment.image != body.image || !moment.image) {
+      const image = request.file('image', this.validationOptions)
+
+      if (image) {
+        const imageName = `${uuidv4()}.${image.extname}`
+
+        await image.move(app.tmpPath('uploads'), {
+          name: imageName,
+        })
+
+        moment.image = imageName
+      }
+    }
+
+    await moment.save()
+
+    return {
+      message: 'Momento atualizado com sucesso!',
       data: moment,
     }
   }
