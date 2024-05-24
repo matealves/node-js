28e0aa04b86d2180840d7f0196baa115cda6f69a
Mateus Alves <contatomateusalves@hotmail.com>
2024-05-24 18:17:23 -0300

    ⚙️feat: create db connection

--- projetos/api-upload-images/server.ts
+++ projetos/api-upload-images/server.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import { dbConnection } from "./src/db/mongoose";
 
 dotenv.config();
 
@@ -10,3 +11,6 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`[PORT:${PORT}] \x1b[32mServidor local iniciado.\x1b[0m\n`);
 });
+
+console.log("Conectando ao MongoDB...");
+const db = dbConnection();
