b216181f1fc815027473ea742434f52129b0b2be
Mateus Alves <contatomateusalves@hotmail.com>
2024-05-24 15:52:31 -0300

    ⚙️chore: create server express

---
+++ projetos/api-upload-images/server.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const server = express();
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`[PORT:${PORT}] \x1b[32mServidor local iniciado.\x1b[0m\n`);
+});
