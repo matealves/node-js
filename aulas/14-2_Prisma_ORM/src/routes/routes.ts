b479bd6c3117728caf1180856f66f7fa50493380
Mateus Alves <contatomateusalves@hotmail.com>
2025-01-17 14:23:14 -0300

    ⚙️chore: add route for get all users

--- aulas/14-2_Prisma_ORM/src/routes/routes.ts
+++ aulas/14-2_Prisma_ORM/src/routes/routes.ts
@@ -5,5 +5,7 @@ import * as apiController from "../controller/api.controller";
 export const router = Router();
 
 router.get("/test", apiController.ping);
+router.get("/users", apiController.getUsers);
+
 router.post("/user", apiController.createNewUser);
 router.post("/users", apiController.createMultipleUser);
