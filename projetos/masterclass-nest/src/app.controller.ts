625b64d2f6b86907e42df875703963588838b2fb
Mateus Alves <contatomateusalves@hotmail.com>
2025-04-22 21:20:10 -0300

    feat: controllers POST and GET

--- projetos/masterclass-nest/src/app.controller.ts
+++ projetos/masterclass-nest/src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './database/prisma.service';
+import { CreateUserBody } from './dtos/create-user-body';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private prisma: PrismaService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('user')
+  async createUser(@Body() body: CreateUserBody) {
+    const { name, function: userFunction } = body;
+
+    const user = await this.prisma.user.create({
+      data: {
+        name,
+        function: userFunction,
+      },
+    });
+    return user;
+  }
+
+  @Get('users')
+  async getUsers(): Promise<CreateUserBody[]> {
+    const users = await this.prisma.user.findMany();
+    return users;
   }
 }
