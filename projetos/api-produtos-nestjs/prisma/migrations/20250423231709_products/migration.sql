bb86c5d4fb388411dd9f273cda79d126622580c6
Mateus Alves <contatomateusalves@hotmail.com>
2025-04-24 09:02:40 -0300

    chore: database

---
+++ projetos/api-produtos-nestjs/prisma/migrations/20250423231709_products/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Products" (
+    "id" SERIAL NOT NULL,
+    "category" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Products_pkey" PRIMARY KEY ("id")
+);
