51692b00547524b3c05e3cafb1f4cbd46113a95a
Mateus Alves <contatomateusalves@hotmail.com>
2025-07-21 20:41:22 -0300

    feat: create DTO to list users

--- projetos/api-loja-nestjs-alura/src/user/user.controller.ts
+++ projetos/api-loja-nestjs-alura/src/user/user.controller.ts
@@ -1,6 +1,9 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { CreateUserDTO } from './dto/CreateUser.dto';
+import { UserEntity } from './user.entity';
+import { v4 as uuid } from 'uuid';
+import { ListUserDTO } from './dto/listUser.dto';
 
 @Controller('/users')
 export class UserController {
@@ -9,12 +12,29 @@ export class UserController {
 
   @Post()
   async createUser(@Body() data: CreateUserDTO) {
-    this.userRepository.createUser(data);
-    return { message: 'User created successfully', data };
+    const userEntity = new UserEntity();
+
+    userEntity.name = data.name;
+    userEntity.lastName = data.lastName;
+    userEntity.email = data.email;
+    userEntity.password = data.password;
+    userEntity.id = uuid();
+
+    this.userRepository.createUser(userEntity);
+    return {
+      message: 'User created successfully',
+      data: {
+        id: userEntity.id,
+        email: userEntity.email,
+      },
+    };
   }
 
   @Get()
   async getUsers() {
-    return this.userRepository.getUsers();
+    const users = await this.userRepository.getUsers();
+    const usersList = users.map((user) => new ListUserDTO(user.id, user.email));
+
+    return usersList;
   }
 }
