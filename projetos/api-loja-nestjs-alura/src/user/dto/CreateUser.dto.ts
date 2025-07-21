62d7c875e80729f7345c299f201add25d0a0d106
Mateus Alves <contatomateusalves@hotmail.com>
2025-07-21 20:47:38 -0300

    refactor: create user DTO

--- projetos/api-loja-nestjs-alura/src/user/dto/CreateUser.dto.ts
+++ projetos/api-loja-nestjs-alura/src/user/dto/CreateUser.dto.ts
@@ -4,11 +4,14 @@ import { EmailIsUnique } from '../validation/email-unique.validator';
 export class CreateUserDTO {
   @IsNotEmpty({ message: 'Name is required' })
   name: string;
+
   @IsNotEmpty({ message: 'LastName is required' })
   lastName: string;
-  @EmailIsUnique({ message: 'Email is already in use' })
+
   @IsEmail(undefined, { message: 'Invalid email' })
+  @EmailIsUnique({ message: 'Email is already in use' })
   email: string;
+
   @MinLength(6, { message: 'Password must be at least 6 characters long' })
   password: string;
 }
