6f97a7ee03208cb874224d6fcd3eb3da4a275ded
Mateus Alves <contatomateusalves@hotmail.com>
2025-07-22 19:49:54 -0300

    refactor: user repository

--- projetos/api-loja-nestjs-alura/src/user/user.repository.ts
+++ projetos/api-loja-nestjs-alura/src/user/user.repository.ts
@@ -19,35 +19,37 @@ export class UserRepository {
     },
   ];
 
-  async createUser(user: UserEntity) {
-    this.users.push(user);
-  }
-
-  async updateUser(id: string, data: Partial<UserEntity>) {
+  private findById(id: string) {
     const existisUser = this.users.find((user) => user.id === id);
 
     if (!existisUser) {
       throw new Error('User not found.');
     }
 
-    Object.entries(data).forEach(([key, value]) => {
-      if (key === 'id' || value === undefined) return;
-
-      existisUser[key] = value;
-    });
-
     return existisUser;
   }
 
-  async removeUser(id: string) {
-    const existisUser = this.users.find((user) => user.id === id);
+  async createUser(user: UserEntity) {
+    this.users.push(user);
+  }
 
-    if (existisUser) {
-      this.users = this.users.filter((user) => user.id !== id);
-      return true;
-    } else {
-      return false;
-    }
+  async updateUser(id: string, data: Partial<UserEntity>) {
+    const user = this.findById(id);
+
+    Object.entries(data).forEach(([key, value]) => {
+      if (key === 'id' || value === undefined) return;
+
+      user[key] = value;
+    });
+
+    return user;
+  }
+
+  async removeUser(id: string) {
+    const user = this.findById(id);
+    this.users = this.users.filter((user) => user.id !== id);
+    
+    return user;
   }
 
   async getUsers() {
