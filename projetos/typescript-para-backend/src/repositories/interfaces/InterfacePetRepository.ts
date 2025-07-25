026487f6fef44edd1db49b63abf5fcf1cbe3b7dd
Mateus Alves <contatomateusalves@hotmail.com>
2025-07-25 20:13:37 -0300

    fix: adjustment interface

--- projetos/typescript-para-backend/src/repositories/interfaces/InterfacePetRepository.ts
+++ projetos/typescript-para-backend/src/repositories/interfaces/InterfacePetRepository.ts
@@ -3,6 +3,9 @@ import PetEntity from "../../entities/PetEntity";
 export default interface InterfacePetRepository {
   criaPet(pet: PetEntity): void;
   listaPet(): Promise<PetEntity[]> | PetEntity[];
-  atualizaPet(id: number, pet: PetEntity): void;
-  deletaPet(id: number, pet: PetEntity): void;
+  atualizaPet(
+    id: number,
+    newData: PetEntity
+  ): Promise<{ success: boolean; message?: string }> | void;
+  deletaPet(id: number): Promise<{ success: boolean; message?: string }> | void;
 }
