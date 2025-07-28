4ba3dc375548fddb676f995ba5da448896baf49e
Mateus Alves <contatomateusalves@hotmail.com>
2025-07-28 20:04:10 -0300

    feat: interface and repository

--- projetos/typescript-para-backend/src/repositories/AdotanteRepository.ts
+++ projetos/typescript-para-backend/src/repositories/AdotanteRepository.ts
@@ -8,4 +8,54 @@ export default class AdotanteRepository implements InterfaceAdotanteRepository {
   async criaAdotante(adotante: AdotanteEntity): Promise<void> {
     await this.repository.save(adotante);
   }
+
+  async listaAdotantes(): Promise<AdotanteEntity[]> {
+    return await this.repository.find();
+  }
+  async atualizaAdotante(
+    id: number,
+    newData: AdotanteEntity
+  ): Promise<{ success: boolean; message?: string }> {
+    try {
+      const adotanteToUpdate = await this.repository.findOne({ where: { id } });
+
+      if (!adotanteToUpdate) {
+        return { success: false, message: "Adotante não encontrado" };
+      }
+
+      Object.assign(adotanteToUpdate, newData);
+
+      await this.repository.save(adotanteToUpdate);
+
+      return { success: true };
+    } catch (error) {
+      console.log(error);
+      return {
+        success: false,
+        message: "Ocorreu um erro ao tentar atualizar o adotante.",
+      };
+    }
+  }
+
+  async deletaAdotante(
+    id: number
+  ): Promise<{ success: boolean; message?: string }> {
+    try {
+      const adotanteToRemove = await this.repository.findOne({ where: { id } });
+
+      if (!adotanteToRemove) {
+        return { success: false, message: "Adotante não encontrado" };
+      }
+
+      await this.repository.remove(adotanteToRemove);
+
+      return { success: true };
+    } catch (error) {
+      console.error(error);
+      return {
+        success: false,
+        message: "Ocorreu um erro ao tentar excluir o adotante.",
+      };
+    }
+  }
 }
--- projetos/typescript-para-backend/src/repositories/interfaces/InterfaceAdotanteRepository.ts
+++ projetos/typescript-para-backend/src/repositories/interfaces/InterfaceAdotanteRepository.ts
@@ -1,6 +1,13 @@
 import AdotanteEntity from "../../entities/AdotanteEntity";
 
 export default interface InterfaceAdotanteRepository {
-  criaAdotante(adotante: AdotanteEntity): void | Promise<void>;
-  // ...outros métodos existentes
+  criaAdotante(adotante: AdotanteEntity): Promise<void>;
+  listaAdotantes(): AdotanteEntity[] | Promise<AdotanteEntity[]>;
+  atualizaAdotante(
+    id: number,
+    adotante: AdotanteEntity
+  ): Promise<{ success: boolean; message?: string }> | void;
+  deletaAdotante(
+    id: number
+  ): Promise<{ success: boolean; message?: string }> | void;
 }
