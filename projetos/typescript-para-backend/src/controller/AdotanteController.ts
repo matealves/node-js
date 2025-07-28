5d39179f24767fbbf6819ba7c43333585f296f14
Mateus Alves <contatomateusalves@hotmail.com>
2025-07-28 19:39:25 -0300

    feat: create controller

---
+++ projetos/typescript-para-backend/src/controller/AdotanteController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import AdotanteEntity from "../entities/AdotanteEntity";
+import AdotanteRepository from "../repositories/AdotanteRepository";
+
+export default class AdotanteController {
+  constructor(private readonly repository: AdotanteRepository) {}
+
+  async criaAdotante(req: Request, res: Response) {
+    try {
+      const { nome, celular, endereco, foto, senha } =
+        req.body as AdotanteEntity;
+
+      const novoAdotante = new AdotanteEntity(
+        nome,
+        senha,
+        celular,
+        foto,
+        endereco
+      );
+
+      await this.repository.criaAdotante(novoAdotante);
+      return res.status(201).json(novoAdotante);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Erro ao criar o adotante" });
+    }
+  }
+}
