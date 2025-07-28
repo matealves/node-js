4028529335500cc53d0f84b546a00a484d657b79
Mateus Alves <contatomateusalves@hotmail.com>
2025-07-28 20:45:54 -0300

    feat: adota pet

--- projetos/typescript-para-backend/src/repositories/interfaces/InterfacePetRepository.ts
+++ projetos/typescript-para-backend/src/repositories/interfaces/InterfacePetRepository.ts
@@ -2,10 +2,18 @@ import PetEntity from "../../entities/PetEntity";
 
 export default interface InterfacePetRepository {
   criaPet(pet: PetEntity): void;
+
   listaPet(): Promise<PetEntity[]> | PetEntity[];
+
   atualizaPet(
     id: number,
     newData: PetEntity
   ): Promise<{ success: boolean; message?: string }> | void;
+
   deletaPet(id: number): Promise<{ success: boolean; message?: string }> | void;
+
+  adotaPet(
+    idPet: number,
+    idAdotante: number
+  ): Promise<{ success: boolean; message?: string }>;
 }
