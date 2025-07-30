594d85aab1f99623afd4cf5a2a112b9b4cc3f434
Mateus Alves <contatomateusalves@hotmail.com>
2025-07-30 09:14:57 -0300

    feat: implements new Enum

--- projetos/typescript-para-backend/src/entities/PetEntity.ts
+++ projetos/typescript-para-backend/src/entities/PetEntity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import EnumEspecie from "../enum/EnumEspecie";
 import AdotanteEntity from "./AdotanteEntity";
+import EnumPorte from "../enum/EnumPorte";
 
 @Entity()
 export default class PetEntity {
@@ -10,22 +11,26 @@ export default class PetEntity {
   nome: string;
   @Column()
   especie: EnumEspecie;
+  @Column({ nullable: true })
+  porte?: EnumPorte;
   @Column()
   dataDeNascimento: Date;
   @Column()
   adotado: boolean;
-  @ManyToOne(()=> AdotanteEntity, (adotante) => adotante.pets)
+  @ManyToOne(() => AdotanteEntity, (adotante) => adotante.pets)
   adotante!: AdotanteEntity;
 
   constructor(
     nome: string,
     especie: EnumEspecie,
     dataDeNascimento: Date,
-    adotado: boolean
+    adotado: boolean,
+    porte?: EnumPorte,
   ) {
     this.nome = nome;
     this.especie = especie;
     this.dataDeNascimento = dataDeNascimento;
     this.adotado = adotado;
+    this.porte = porte;
   }
 }
