6859c0727fa9c027c1f62326504f0eb0e879b606
Mateus Alves <contatomateusalves@hotmail.com>
2025-07-30 09:15:45 -0300

    feat: create new router filter

--- projetos/typescript-para-backend/src/routes/petRouter.ts
+++ projetos/typescript-para-backend/src/routes/petRouter.ts
@@ -15,6 +15,11 @@ router.get("/", (req, res) => petController.listaPets(req, res));
 router.post("/", (req, res) => petController.criaPet(req, res));
 router.put("/:id", (req, res) => petController.atualizaPet(req, res));
 router.delete("/:id", (req, res) => petController.deletaPet(req, res));
-router.put("/:pet_id/:adotante_id", (req, res) => petController.adotaPet(req, res));
+router.put("/:pet_id/:adotante_id", (req, res) =>
+  petController.adotaPet(req, res)
+);
+router.get("/filtroPorte", (req, res) =>
+  petController.buscaPetPeloPorte(req, res)
+);
 
 export default router;
