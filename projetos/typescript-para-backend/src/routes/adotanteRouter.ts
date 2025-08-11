781a47baf9dcabdf2e5aa3d39de4b0941cdbcf4d
Mateus Alves <contatomateusalves@hotmail.com>
2025-08-11 17:49:01 -0300

    feat: insert middleware

--- projetos/typescript-para-backend/src/routes/adotanteRouter.ts
+++ projetos/typescript-para-backend/src/routes/adotanteRouter.ts
@@ -2,7 +2,8 @@ import express, { RequestHandler } from "express";
 import AdotanteController from "../controller/AdotanteController";
 import AdotanteRepository from "../repositories/AdotanteRepository";
 import { AppDataSource } from "../config/dataSource";
-import middlewareValidatorBodyAdotante from "../middleware/adotandeRequestBody";
+import { middlewareValidatorBodyAdotante } from "../middlewares/adotandeRequestBody";
+import { middlewareValidatorBodyEndereco } from "../middlewares/enderecoRequestBody";
 
 const router = express.Router();
 const adotanteRepository = new AdotanteRepository(
@@ -13,6 +14,9 @@ const adotanteController = new AdotanteController(adotanteRepository);
 const validateBodyAdotante: RequestHandler = (req, res, next) =>
   middlewareValidatorBodyAdotante(req, res, next);
 
+const validateBodyEndereco: RequestHandler = (req, res, next) =>
+  middlewareValidatorBodyEndereco(req, res, next);
+
 router.post("/", validateBodyAdotante, (req, res) =>
   adotanteController.criaAdotante(req, res)
 );
@@ -21,7 +25,7 @@ router.put("/:id", (req, res) => adotanteController.atualizaAdotante(req, res));
 router.delete("/:id", (req, res) =>
   adotanteController.deletaAdotante(req, res)
 );
-router.patch("/:id", (req, res) =>
+router.patch("/:id", validateBodyEndereco, (req, res) =>
   adotanteController.atualizaEnderecoAdotante(req, res)
 );
 
