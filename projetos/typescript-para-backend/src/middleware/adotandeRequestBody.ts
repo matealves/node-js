d0d7f56819a8082e099bfcf712e9f5712bd49124
Mateus Alves <contatomateusalves@hotmail.com>
2025-08-11 17:34:07 -0300

    feat: create middleware validator

---
+++ projetos/typescript-para-backend/src/middleware/adotandeRequestBody.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from "express";
+import * as yup from "yup";
+import { TipoRequestBodyAdotante } from "../types/tiposAdotante";
+
+const schemaBodyAdotante: yup.ObjectSchema<
+  Omit<TipoRequestBodyAdotante, "endereco">
+> = yup.object({
+  nome: yup.string().defined().required(),
+  celular: yup.string().defined().required(),
+  senha: yup
+    .string()
+    .defined()
+    .required()
+    .min(6, "Senha deve ter pelo menos 6 caracteres"),
+  foto: yup.string().optional(),
+});
+
+const middlewareValidatorBodyAdotante = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    await schemaBodyAdotante.validate(req.body, { abortEarly: false });
+    return next();
+  } catch (error) {
+    const yupErros = error as yup.ValidationError;
+
+    const validationErros: Record<string, string> = {};
+
+    yupErros.inner.forEach((error) => {
+      if (!error.path) return;
+      validationErros[error.path] = error.message;
+    });
+
+    return res.status(400).json({ error: validationErros });
+  }
+};
+
+export default middlewareValidatorBodyAdotante;
