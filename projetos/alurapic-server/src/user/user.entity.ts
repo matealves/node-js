3d721d019ae8e859dd5e8f653a320d6884f3bb67
Mateus Alves <contatomateusalves@hotmail.com>
2025-09-16 10:09:12 -0300

    chore: config class validator

--- projetos/alurapic-server/src/user/user.entity.ts
+++ projetos/alurapic-server/src/user/user.entity.ts
@@ -1,14 +1,17 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsUserNameUnique } from './is-user-name-unique.validator';
 
 export class User {
   id: number;
 
   @IsNotEmpty()
   @IsString()
+  @IsUserNameUnique({ message: 'Username must be unique' })
   username: string;
 
   @IsEmail()
   email: string;
+
   @IsNotEmpty()
   password: string;
 
--- projetos/alurapic-server/src/user/user.module.ts
+++ projetos/alurapic-server/src/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { IsUserNameUniqueConstraint } from './is-user-name-unique.validator';
 
 @Module({
   imports: [],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, IsUserNameUniqueConstraint],
 })
 export class UserModule {}
