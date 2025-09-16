889e6d8010140e2a69194dc0d53fad81b0cd4707
Mateus Alves <contatomateusalves@hotmail.com>
2025-09-16 10:55:55 -0300

    feat: serializer

--- projetos/alurapic-server/src/user/user.entity.ts
+++ projetos/alurapic-server/src/user/user.entity.ts
@@ -1,5 +1,6 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { IsUserNameUnique } from './is-user-name-unique.validator';
+import { Exclude, Expose } from 'class-transformer';
 
 export class User {
   id: number;
@@ -12,8 +13,12 @@ export class User {
   @IsEmail()
   email: string;
 
+  @Expose({
+    name: 'password',
+  })
+  @Exclude({ toPlainOnly: true })
   @IsNotEmpty()
-  password: string;
+  senha: string;
 
   @IsNotEmpty()
   fullName: string;
